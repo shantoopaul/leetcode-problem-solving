f6250817366230edc7cb9f954fad088e7fbb9752
Shanto Paul <shantoopaul@gmail.com>
2026-02-04 21:48:32 +0600

    Time: 1 ms (83.21%) | Memory: 59 MB (21.69%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    let res = [];
+    for (let i = 0; i < words.length; i++) {
+        words[i].includes(x) && res.push(i);
+    }
+    return res;
+};
