71ae1447b6a47f89fbc8ff84affb0129fd97b9f6
Shanto Paul <shantoopaul@gmail.com>
2026-02-05 16:09:41 +0600

    Time: 0 ms (100.00%) | Memory: 19.6 MB (16.49%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,6 +1,7 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = []
-        for num in range(len(nums)):
-            ans.append(nums[nums[num]])
-        return ans
+        # ans = []
+        # for num in range(len(nums)):
+        #     ans.append(nums[nums[num]])
+        # return ans
+        return list(map(lambda n: nums[n], nums))
