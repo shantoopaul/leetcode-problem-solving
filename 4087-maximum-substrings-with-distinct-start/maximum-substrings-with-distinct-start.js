6bc9bcf27be9e0c53b832af9a9aa7565e6831c83
Shanto Paul <shantoopaul@gmail.com>
2026-02-05 13:59:16 +0600

    Time: 54 ms (18.01%) | Memory: 64.1 MB (43.55%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.js
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.js
@@ -2,11 +2,4 @@
  * @param {string} s
  * @return {number}
  */
-var maxDistinct = function(s) {
-    // let res = new Set();
-    // for (let x of s) {
-    //     res.add(x)
-    // }
-    // return res.size;
-    return new Set(s).size
-};
+const maxDistinct = (s) => new Set(s).size
