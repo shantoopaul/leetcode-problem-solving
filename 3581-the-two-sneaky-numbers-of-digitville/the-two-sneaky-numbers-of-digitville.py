c31f286db684cb0c6fd510b46aae80aef87bb0e5
Shanto Paul <shantoopaul@gmail.com>
2026-02-06 16:00:16 +0600

    Time: 3 ms (44.93%) | Memory: 19.4 MB (13.84%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -1,10 +1,9 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        x = set()
-        y = set()
-        for i in nums:
-            if i in x:
-                y.add(i)
-            else:
-                x.add(i)
-        return list(y)
+        nums.sort()
+        arr = []
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                arr.append(nums[i])
+        return arr
